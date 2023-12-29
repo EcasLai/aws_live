b79e48468b824913d5a9a0c0b0a46c530e21bb09
EcasLai <Ecas.lai@outlook.com>
2023-12-29 19:55:59 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cyn5lp3xkp6r.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "laicheesheng-employee"
+customregion = "us-east-1"
+
